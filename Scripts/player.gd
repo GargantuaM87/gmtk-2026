7ae74502c6db2aed167b7474b79d47d6988e326b
Micah S <145581856+Zeonthestupid@users.noreply.github.com>
2026-07-24 13:03:04 -0600

    what

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@export var animated_sprite_2d: AnimatedSprite2D
+@onready var sprite2D = $Node2D/AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -8,29 +8,37 @@ const HORIZONTAL_VELOCITY = 350
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-	if is_on_floor():
-		velocity.y = 0
-		if Input.is_action_pressed("space"):
-			velocity.y = JUMP_VELOCITY
-	var dir = 0
-	if Input.is_action_pressed("d_button"):
-		animated_sprite_2d.play("running")
-		dir += 1
-	if Input.is_action_pressed("a_button"):
-		animated_sprite_2d.play("running")
-		dir -= 1
-	if !Input.is_action_pressed("a_button") and !Input.is_action_pressed("d_button"):
-		animated_sprite_2d.play("default")
+	
+	#Jump Action
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Debug Action???
 	if Input.is_action_just_pressed("debug1"):
 		var floating = preload("res://Scenes/text_handler.tscn").instantiate()
 		get_tree().current_scene.add_child(floating)
 		floating.show_text("+10s", position - Vector2(0, 65))
-	if dir == 1:
-		$Node2D.scale.x = 1
-	elif dir == -1:
-		$Node2D.scale.x = -1
+		
+	var dir := Input.get_axis("a_button", "d_button")
+	
+	if dir:
+		velocity.x = dir * SPEED
+		if dir > 0:
+			sprite2D.flip_h = false
+		else:
+			sprite2D.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if is_on_floor():
+		# Necessary animations to play
+		if dir == 0:
+			sprite2D.play("default")
+		else:
+			sprite2D.play("running")
+	else:
+		sprite2D.play("jumping")	
+	
 	if Input.is_action_just_pressed("mouse_left"):
-		%Sword.hit_horizontal()
-	velocity.x = dir * HORIZONTAL_VELOCITY
+		pass
 	move_and_slide()
 	
