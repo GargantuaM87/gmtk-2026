420c6f2cf335204cd30e3bed15cf2d870a6f72cd
NkosiDaniel <xxxenouniverse@gmail.com>
2026-07-26 03:00:04 -0400

    Following the previous commit

---
+++ Scripts/kill_box.gd
@@ -0,0 +1,26 @@
+extends Area2D
+signal activated
+var bodies_in_hitbox: Array = []
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+var started:bool = false
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+		
+
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("player"):
+		print("kitbox in")
+		bodies_in_hitbox.append(body)
+		player.kill()
+		
+		
+		
---
+++ Scripts/levelManager.gd
@@ -0,0 +1,45 @@
+extends Node2D
+var rng = RandomNumberGenerator.new()
+@export var scenes: Array[PackedScene]
+@export var rooms: int
+@onready var room_manager: Marker2D = $"."
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+var loaded: Array[Node2D] = []
+var loadednums: Array[int] = []
+
+func genint():
+	var rand = rng.randi_range(0, scenes.size()-1)
+	return rand
+	
+func loadscene(target_exit: Vector2):
+	var rand = 0
+	while true:
+		rand = genint()
+		if loadednums.size() > scenes.size():
+			print("flushedloadednums")
+			loadednums.clear()
+		if rand not in loadednums:
+			loadednums.append(rand)
+			break
+	var room = scenes[rand].instantiate()
+	loaded.append(room)
+	add_child(room)
+	
+	var new_entrance: Vector2 = room.get_node("EntryPoint").global_position
+	# Calculate offset and shift the new room
+	var offset: Vector2 = target_exit - new_entrance
+	room.global_position += offset
+	
+	
+func loadRooms():
+	loadscene(room_manager.global_position)
+	for i in (rooms - 1):
+		loadscene(loaded[-1].get_node("ExitPoint").global_position)
+		
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
